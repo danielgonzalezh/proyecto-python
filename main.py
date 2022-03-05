0429a88840d096be23df02bbb331cd5061a5a2ce
206 <206@academicos.cesde.edu.co>
2022-03-04 19:01:17 -0500

    NTV002 se crea programa

---
+++ main.py
@@ -0,0 +1,19 @@
+
+# ejemplo de estaciones
+mes = input("Digita un mes del año ")
+
+
+print(f"El Mes digitado fue {mes}")
+
+
+if(mes == "diciembre" or mes =="enero" or mes =="febrero" or mes =="marzo"):
+    print("Estas en INVIERNO")
+elif( mes == "junio" or mes == "julio" or mes == "agosto"):
+    print("Estas en VERANO")
+elif( mes == "abril" or mes == "mayo"):
+    print("Estas en PRIMAVERA")
+elif( mes == "septiembre" or mes == "octubre" or mes == "noviembre"):
+    print("Estas en OTOÑO")
+else:
+    print ("NO EXISTE")
+
